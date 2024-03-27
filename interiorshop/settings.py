d923ead0c3e786ba67e53ed8598e4779387b222e
unknown <jhyu0210@gmail.com>
2024-03-27 11:33:38 +0900

    change db to vercel postgres

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -98,23 +98,23 @@ if not DEBUG:
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ["DB_NAME"],
-#         # 'django-multivendorshop-postgres', 
-#         'USER': os.environ["DB_USER"],
-#         'PASSWORD': os.environ["DB_PASSWORD"],
-#         'HOST': os.environ["DB_HOST"],
-#         'PORT': os.environ["DB_PORT"],
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["DB_NAME"],
+        # 'django-multivendorshop-postgres', 
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"],
+        'HOST': os.environ["DB_HOST"],
+        'PORT': os.environ["DB_PORT"],
+    }
+}
 
 
 # Password validation
